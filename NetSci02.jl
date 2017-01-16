39edb0fc4697bb9ec236bfcc3a56d7234e57a715
Philipp Gabler <phips.gabler@gmail.com>
2017-01-16 17:45:15 +0100

    Initial commit

---
+++ NetSci02.jl
@@ -0,0 +1,46 @@
+module NetSci01
+
+using LightGraphs, GraphPlot
+using Distributions
+
+export readnetwork
+
+const line_regex = r"^(\d+)\s(\d+)"
+
+"Read a space separated file into an (undirected) Graph in an efficient way."
+function readnetwork(filename::String, limit::Number = Inf; fromzero::Bool = false)
+    graph = Graph()
+    vertices = 0
+    correction = Int(fromzero)
+
+    # using grep to exclude comment lines
+    open(filename) do file
+        for (l, line) in enumerate(eachline(file))
+            if l > limit
+                break
+            end
+
+            if (m = match(line_regex, line)) !== nothing
+                raw_v1, raw_v2 = m.captures
+                v1 = parse(Int, raw_v1) + correction
+                v2 = parse(Int, raw_v2) + correction
+            
+                if v1 > vertices || v2 > vertices
+                    @assert add_vertices!(graph, max(v1, v2) - vertices)
+                    vertices = max(v1, v2)
+                end
+                
+                # we explicitely ignore inverse directions, if there are any
+                @assert has_edge(graph, v1, v2) || add_edge!(graph, v1, v2)
+            end
+        end
+    end
+
+    return graph
+end
+
+
+
+
+
+end
