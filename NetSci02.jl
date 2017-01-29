278b3b52db5236d648689e8b23ebee8f85b8cf22
Philipp Gabler <phips.gabler@gmail.com>
2017-01-29 16:13:23 +0100

    Fix probability normalization problem

--- NetSci02.jl
+++ NetSci02.jl
@@ -37,6 +37,8 @@ function samplepercolations(measure::Function,
             end
             
             curves[r, i] = measure(victim)
+            # @show r, φ, connected_components(victim)
+            # @show distributions[[1, 10]]
         end
     end
     
@@ -44,43 +46,47 @@ function samplepercolations(measure::Function,
 end
 
 
-# """
-#     informed_probabilities(graph::Graph, weights::Vector{Float64})
+"""
+    informed_probabilities(graph::Graph, weights::Vector{Float64}, infos::Vector{Symbol})
 
-# Construct a distribution over nodes which integrates knowledge about local clustering,
-# degree, and second-order neighbourhood, taking into account `weights`.
-# """
-# function informed_probabilities(graph::Graph, weights::Vector{Float64},
-#                                 infos::Vector{Symbol})::Function
-#     const maxdegrees = nv(graph) * (nv(graph) + 1) / 2
-#     const information_functions = Dict(
-#         :local_clustering => g -> local_clustering_coefficient(g),
-#         :degree => g -> degree(g) / maxdegrees,
-#         :neighbourhood2 => g -> map(v -> length(neighborhood(g, v, 2)), vertices(g)) / maxdegrees,
-#         :neighbourhood3 => g -> map(v -> length(neighborhood(g, v, 3)), vertices(g)) / maxdegrees
-#     )
+Construct a paramtrized vector of distributions over nodes which integrates knowledge about local
+clustering, degree, and second-order neighbourhood, taking into account `weights`.  `infos` can
+be used to choose which information to include.
+"""
+function informed_probabilities(graph::Graph,
+                                weights::Vector{Float64},
+                                infos::Vector{Symbol})::Function
+    const maxdegrees = nv(graph) * (nv(graph) + 1) / 2
+    const information_functions = Dict(
+        :degree => g -> degree(g) / maxdegrees,
+        :local_clustering => g -> local_clustering_coefficient(g),
+        :neighbourhood2 => g -> map(v -> length(neighborhood(g, v, 2)), vertices(g)) / maxdegrees,
+        :neighbourhood3 => g -> map(v -> length(neighborhood(g, v, 3)), vertices(g)) / maxdegrees
+    )
     
-#     information = Array(Float64, nv(graph), length(infos))
-#     for (i, info) in enumerate(infos)
-#         information[:, i] = information_functions[info](graph)
-#     end
+    information = Array(Float64, nv(graph), length(infos))
+    for (i, info) in enumerate(infos)
+        information[:, i] = information_functions[info](graph)
+    end
 
-#     probabilities = logistic.(information * weights)
-#     mean_probability = mean(probabilities)
-#     return φ -> Bernoulli.(probabilities - mean_probability + φ)
-# end
+    probabilities = logistic.(information * weights)
+    return function(φ)
+        scaled_probabilitiess = probabilities .* (φ / mean(probabilities))
+        Bernoulli.(clamp(scaled_probabilitiess, 0.0, 1.0)) # because rounding errors
+    end
+end
 
 
 """
-    percolation_loss(graph, samples)
+    percolation_loss(graph::Graph, infos::Vector{Symbol}, samples, stepsize)
 
 Construct a loss function which samples the expected area of the percolation curve of `graph`,
-using the parameter as weightings.
+using the parameter as weightings for local information.
 """
 function percolation_loss(graph::Graph, infos::Vector{Symbol}, samples = 10, stepsize = 0.05)
     function loss(parameter)
-        dist(g) = informed_probabilities(g, parameter, infos)
-        curves = samplepercolations(graph, samples, dist) do g
+        dist = informed_probabilities(graph, parameter, infos)
+        curves = samplepercolations(graph, dist, samples, stepsize) do g
             reduce(max, 0, length.(connected_components(g)))
         end
 
@@ -91,46 +97,41 @@ function percolation_loss(graph::Graph, infos::Vector{Symbol}, samples = 10, ste
 end
 
 
-normalize_curve(curve) = curve / maximum(curve)
-percolation_area(curves, stepsize) = sum(curves ./ mapslices(maximum, curves, 2)) * stepsize / size(curves)[2]
+# normalize_curve(curve) = curve / maximum(curve)
+# percolation_area(curves, stepsize) = sum(curves ./ mapslices(maximum, curves, 2)) * stepsize / size(curves)[2]
 
-function simulate_random(graph = Graph(1000, 2000))
-    const stepsize = 0.05
-    results = samplepercolations(graph, φ -> fill(Bernoulli(φ), nv(graph)), 10, stepsize) do g
-        reduce(max, 0, length.(connected_components(g)))
-    end
-
-    println(percolation_area(results, stepsize))
-    println(results[1, :])
-    plot(0.0:stepsize:1.0, transpose(results))
-end
-
-
-# function test(;nv = 100, ne = 50)
-#     graph = Graph(nv, ne)
-#     # samplepercolations(graph, 10, _ -> DiscreteUniform(1, nv)) do g
-#     #     length(connected_components(g)), length(triangles(g))
-#     # end
-
-#     error_trace = Float64[]
-    
-#     function log_annealing(;kwargs...)
-#         args = Dict(kwargs)
-#         push!(error_trace, args[:f_new])
-        
-#         if args[:steps] % 10 == 0
-#             println(args[:steps], " steps, T = ", args[:T])
-#             println("\t", args[:x_new])
-#             println("\t", args[:f_new])
-#         end
+# function simulate_random(graph = Graph(1000, 2000))
+#     const stepsize = 0.05
+#     results = samplepercolations(graph, φ -> fill(Bernoulli(φ), nv(graph)), 10, stepsize) do g
+#         reduce(max, 0, length.(connected_components(g)))
 #     end
 
-#     infos = [:degree]
-#     annealing(percolation_loss(graph, 30, infos), randn(length(infos)),
-#               1.0, 0.99, 0.05, (s, v) -> s > 1000;
-#               debug_callback = log_annealing)
-
-#     plot(error_trace)
+#     println(percolation_area(results, stepsize))
+#     println(results[1, :])
+#     plot(0.0:stepsize:1.0, transpose(results))
 # end
+
+
+function test(graph = Graph(100, 300))
+    error_trace = Float64[]
+    
+    function log_annealing(;kwargs...)
+        args = Dict(kwargs)
+        push!(error_trace, args[:f_new])
+        
+        if args[:steps] % 10 == 0
+            println(args[:steps], " steps, T = ", args[:T])
+            println("\tParameters: ", args[:x_new])
+            println("\tError: ", args[:f_new])
+        end
+    end
+
+    infos = [:degree]
+    annealing(percolation_loss(graph, infos), randn(length(infos)),
+              10000.0, 0.99, 10, (s, v) -> s > 10000;
+              debug_callback = log_annealing)
+
+    plot(error_trace)
+end
 
 end
