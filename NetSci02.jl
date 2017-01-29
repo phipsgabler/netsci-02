552d4d4acad54b8e7dc60b2d4b4900a3794d5680
Philipp Gabler <phips.gabler@gmail.com>
2017-01-29 14:43:38 +0100

    Fix area calculation

--- NetSci02.jl
+++ NetSci02.jl
@@ -8,33 +8,35 @@ include("utils.jl")
 
 
 """
-    samplepercolations(measure::Function, graph::Graph, repetitions::Integer,
-                       node_distributions::Vector{F}, range)
+    samplepercolations(measure::Function, graph::Graph, node_distributions::Function,
+                       repetitions::Integer, stepsize = 0.1)
 
-Sample `repetitions` node removals on `graph`, calculating `measure` each time.  The
-probability of deleting a nodes is calculated by `node_distribution(φ)` for each φ.
+Sample `repetitions` node removals on `graph`, over deletion rates `0.0:stepsize:1.0`, calculating
+`measure` each time.  The probability of deleting a nodes is given by `node_distributions(φ)`
+for each φ (so `node_distributions` must have type Float -> Vector{Bernoulli}).
 """
-function samplepercolations{F<:Function}(measure::Function,
-                                         graph::Graph,
-                                         repetitions::Integer,
-                                         node_distributions::Vector{F},
-                                         range = 0.1:0.1:1.0,
-                                         result_type = typeof(measure(Graph(1, 0))))
-    @assert all(map(≈, extrema(range), (0.0, 1.0)))
-    
+function samplepercolations(measure::Function,
+                            graph::Graph,
+                            node_distributions::Function,
+                            repetitions::Integer,
+                            stepsize = 0.1,
+                            result_type = typeof(measure(Graph(1, 0))))
     const vertices = nv(graph)
-    curves = SharedArray(result_type, length(range), repetitions)
+    const range = 0.0:stepsize:1.0
+    curves = SharedArray(result_type, repetitions, length(range))
     
-    @sync for (i, φ) in enumerate(range)
-        @parallel for r in 1:repetitions
+    pmap(1:repetitions) do r
+        for (i, φ) in enumerate(range)
             victim = copy(graph)
+            distributions = node_distributions(φ)
+            
             for v = vertices:-1:1
-                if rand(node_distributions[v](φ)) == 1
+                if rand(distributions[v]) == 1
                     rem_vertex!(victim, v)
                 end
             end
             
-            curves[i, r] = measure(victim)
+            curves[r, i] = measure(victim)
         end
     end
     
@@ -49,7 +51,7 @@ end
 # degree, and second-order neighbourhood, taking into account `weights`.
 # """
 # function informed_probabilities(graph::Graph, weights::Vector{Float64},
-#                                 infos::Vector{Symbol})::Vector{Bernoulli}
+#                                 infos::Vector{Symbol})::Function
 #     const maxdegrees = nv(graph) * (nv(graph) + 1) / 2
 #     const information_functions = Dict(
 #         :local_clustering => g -> local_clustering_coefficient(g),
@@ -63,41 +65,44 @@ end
 #         information[:, i] = information_functions[info](graph)
 #     end
 
-#     Bernoulli.(logistic.(information * weights))
+#     probabilities = logistic.(information * weights)
+#     mean_probability = mean(probabilities)
+#     return φ -> Bernoulli.(probabilities - mean_probability + φ)
 # end
 
 
-# """
-#     percolation_loss(graph, samples)
+"""
+    percolation_loss(graph, samples)
 
-# Construct a loss function which samples the expected area of the percolation curve of `graph`,
-# using the parameter as weightings.
-# """
-# function percolation_loss(graph::Graph, samples::Integer, infos::Vector{Symbol})
-#     function loss(parameter)
-#         dist(g) = informed_probabilities(g, parameter, infos)
-#         curves = samplepercolations(graph, samples, dist) do g
-#             maximum(length(c) for c in connected_components(g))
-#         end
+Construct a loss function which samples the expected area of the percolation curve of `graph`,
+using the parameter as weightings.
+"""
+function percolation_loss(graph::Graph, infos::Vector{Symbol}, samples = 10, stepsize = 0.05)
+    function loss(parameter)
+        dist(g) = informed_probabilities(g, parameter, infos)
+        curves = samplepercolations(graph, samples, dist) do g
+            reduce(max, 0, length.(connected_components(g)))
+        end
 
-#         normalizers = mapslices(maximum, curves, 2)
-#         # every curve is normed relative to its maximum (highest) value; then, average
-#         sum(curves ./ normalizers) / samples
-#     end
-# end
-
-
+        # normalize by maximum value, then integrate and average
+        normalized_curves = curves ./ mapslices(maximum, curves, 2)
+        return sum(normalized_curves) * stepsize / samples
+    end
+end
 
 
-function simulate_random(;vertices = 1000, edges = 10000)
-    graph = Graph(vertices, edges)
-    range = 0.0:0.1:1.0
-    
-    results = samplepercolations(graph, 10, fill(φ -> Bernoulli(φ), vertices), range) do g
+normalize_curve(curve) = curve / maximum(curve)
+percolation_area(curves, stepsize) = sum(curves ./ mapslices(maximum, curves, 2)) * stepsize / size(curves)[2]
+
+function simulate_random(graph = Graph(1000, 2000))
+    const stepsize = 0.05
+    results = samplepercolations(graph, φ -> fill(Bernoulli(φ), nv(graph)), 10, stepsize) do g
         reduce(max, 0, length.(connected_components(g)))
     end
 
-    plot(range, results)
+    println(percolation_area(results, stepsize))
+    println(results[1, :])
+    plot(0.0:stepsize:1.0, transpose(results))
 end
 
 
