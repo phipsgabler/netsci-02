8a42dc7ed350efe4bcff96ab1a9da041ed4373b2
Philipp Gabler <phips.gabler@gmail.com>
2017-02-02 18:57:33 +0100

    Produce results figure

--- evaluation/evaluation.r
+++ evaluation/evaluation.r
@@ -1,7 +1,17 @@
 library(dplyr)
 library(ggplot2)
 
-read.table('results.txt', col.names = c('network', 'phase', 'p', 'value')) %>%
+## read.table('results_unnormalized.txt', col.names = c('network', 'phase', 'p', 'value')) %>%
+read.table('results1.txt', col.names = c('sample', 'variant', 'phase', 'p', 'value', 'value_smooth')) %>%
+    ## filter(phase == 'optimized') %>%
+    ggplot(aes(p, value_smooth, color = phase)) +
+    facet_grid(variant~sample) +
+    geom_line() +
+    labs(x = 'Expected remaining nodes', y = 'Relative size of largest cluster')
+
+read.table('results2.txt', col.names = c('sample', 'variant', 'phase', 'p', 'value', 'value_smooth')) %>%
+    ## filter(phase == 'optimized') %>%
     ggplot(aes(p, value, color = phase)) +
-    facet_wrap(~network) +
-    geom_line()
+    facet_grid(variant~sample) +
+    geom_line() +
+    labs(x = 'Expected remaining nodes', y = 'Relative size of largest cluster')
